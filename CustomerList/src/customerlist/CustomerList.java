477edbc1c19c2cbd4081ae32adab1ce5f10b383c
PoisonedMinds <steven_biro@hotmail.com>
2015-03-23 09:26:43 -0400

    commenting

--- CustomerList/src/customerlist/CustomerList.java
+++ CustomerList/src/customerlist/CustomerList.java
@@ -10,23 +10,23 @@ import org.apache.commons.lang3.StringUtils;
  * @author Steven Biro
  * @teacher Mr. J. Carron
  * @date 11-Mar-2015 10:51:46 AM
- * @purpose The purpose of this program is to
+ * @purpose The purpose of this program is to store information for a company
  */
 public class CustomerList {
 
     public static void main(String[] args) {
         char code;
         String info = "", numofpeople, postalcode;
-        int test, num = 0, count,x=0;
-        ArrayList list, list2 = new ArrayList();
-        list = fileRead.read("list.secure");
-        for (Object list1 : list) {
+        int test, num = 0, count;
+        ArrayList list, list2 = new ArrayList();//read list and store it in a list array
+        list = fileRead.read("list.secure");//use my fileRead class to read the file
+        for (Object list1 : list) {//add all the info to a single varible formated to have lines
             info += (list1 + "\n");
         }
-        JOptionPane.showMessageDialog(null, info);
+        JOptionPane.showMessageDialog(null, info);//display all the current info
         test = JOptionPane.showConfirmDialog(null, "Do you want to add info to the list of customers?");
         if (test == 0) {
-            numofpeople = JOptionPane.showInputDialog("How many people would you like to add to the list?");
+            numofpeople = JOptionPane.showInputDialog("How many people would you like to add to the list?");//ask how many people the user would like to add to the list
             try {
                 num = Integer.parseInt(numofpeople);
             } catch (NumberFormatException e) {
@@ -35,19 +35,18 @@ public class CustomerList {
             }
             for (int i = 0; i < num; i++) {
                 do {
+                    test = 0;
                     info = JOptionPane.showInputDialog("Input the info in this format:\nfirst name,last name,address,city,province,postal code");
-                    count = StringUtils.countMatches(info, ",");
+                    count = StringUtils.countMatches(info, ",");//make sure the format given is correct
                     if (info == null) {
-                        test = 1;
-                        System.out.println("please input information next time.");
-                        break;
+                        break;//skip the person if the use presses cancel, or subtract 1 of the amount of people to be added to the list
                     } else if (count == 5) {
 
                         postalcode = info.substring(info.length() - 6);
-                        for (int n = 0; n < 6; n++) {
+                        for (int n = 0; n < 6; n++) {//check postal code
                             code = postalcode.charAt(n);
-                            if (n % 2 == 0) {
-                                
+                            if (n % 2 == 0) {//make sure even spots are letters and odd are numbers 
+
                                 if (!Character.isLetter(code)) {
                                     test = 1;
                                     System.out.println("please input the postal code in the correct format");
@@ -61,25 +60,23 @@ public class CustomerList {
                                 }
                             }
                         }
-                        if (test != 1) {
-                            test = 0;
+                        if (test != 1) {//if the given info passes all tests, then add it to the second list
                             list2.add(i, info);
                         }
                     } else {
-                        test = 1;
+                        test = 1;// if the given info is in the incorrect format let the user know
                         System.out.println("please input the info in the correct format next time.");
-                        break;
                     }
 
-                } while (test == 1);
+                } while (test == 1);//retry if there is a problem with the given info
             }
-            fileAdd.add(list2, "list.secure");
-            list = fileRead.read("list.secure");
+            fileAdd.add(list2, "list.secure");// use my fileAdd class to add the second list to the end of te file
+            list = fileRead.read("list.secure");//use my fileRead to refresh the contents of the first list
             info = "";
             for (Object list1 : list) {
                 info += (list1 + "\n");
             }
-            JOptionPane.showMessageDialog(null, info);
+            JOptionPane.showMessageDialog(null, info);//output all the info, including the newly added stuff
         } else {
             System.exit(0);
         }
